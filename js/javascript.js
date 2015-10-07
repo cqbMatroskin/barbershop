57569017843c5bf33c8cbe0be0099df6379e9293
cqbMatroskin <cqb.matroskin@gmail.com>
2015-10-07 16:28:25 +0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,68 @@
+
+		var logon = document.querySelector(".login");
+		var popup = document.querySelector(".modal-content");
+		var close = document.querySelector(".icon-cancel");
+		var form = popup.querySelector("form");
+		var login = popup.querySelector("[name='login']");
+		var password = popup.querySelector("[name='password']");
+		var storage = localStorage.getItem("login");
+
+
+		logon.addEventListener("click", function(event) {
+			event.preventDefault();
+			popup.classList.add("modal-content-show");
+			if (storage) {
+				login.value = storage;
+				password.focus();
+			} else {				
+				login.focus();
+			}
+		});
+
+		close.addEventListener("click", function(event) {
+			event.preventDefault();
+			popup.classList.remove("modal-content-show");
+			popup.classList.remove("modal-error");
+		});
+
+		form.addEventListener("submit", function(event) {
+			if (!(login.value && password.value)) {
+				event.preventDefault();
+				popup.classList.remove("modal-error");
+				popup.offsetWidth = popup.offsetWidth;
+				popup.classList.add("modal-error");
+			} else {
+				localStorage.setItem("login", login.value);
+			}
+		});
+
+		window.addEventListener("keydown", function(event) {
+			if (event.keyCode == 27) {
+				if (popup.classList.contains("modal-content-show")) {
+					popup.classList.remove("modal-content-show");
+					popup.classList.remove("modal-error");
+				}
+			}
+		});
+
+		var mapOpen = document.querySelector(".open-map");
+		var mapPopup = document.querySelector(".modal-content-map");
+		var mapClose = mapPopup.querySelector(".icon-cancel");
+
+		mapOpen.addEventListener("click", function(event) {
+			event.preventDefault();
+			mapPopup.classList.add("modal-content-show");
+		});
+
+		mapClose.addEventListener("click", function(event) {
+			event.preventDefault();
+			mapPopup.classList.remove("modal-content-show");
+		});
+
+		window.addEventListener("keydown", function(event) {
+			if (event.keyCode == 27) {
+				if (mapPopup.classList.contains("modal-content-show")) {
+					mapPopup.classList.remove("modal-content-show");
+				}
+			}
+		});
